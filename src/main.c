1e83fe46f3f4dbbb4ff39529bc05b47808213136
hungicviet <hungnt@icviet.com>
2016-02-27 00:57:48 +0700

    Fix Makefile, GPIOC is working

--- src/main.c
+++ src/main.c
@@ -15,16 +15,9 @@
  */
 
 #include "config.h"
-#include <stdlib.h>
-#include <errno.h>
-//#include <stdio.h>
-//#include <unistd.h>
 #include <libopencm3/stm32/rcc.h>
 #include <libopencm3/stm32/gpio.h>
-//#include <libopencm3/usb/usbd.h>
-//#include <libopencm3/usb/cdc.h>
-//#include "cdcacm.h"
-//#include "tick.h"
+
 #include "debug.h"
 
 void clock_init(void);
@@ -32,13 +25,6 @@ void clock_init(void);
 int main(void)
 {
   clock_init();
- // tick_init();
-
- // cdcacm_init();
-  //tick_wait_ms(5000);
-
-  //int32_t main_tick = tick_get();
-
   gpio_mode_setup(GPIOC, GPIO_MODE_OUTPUT, GPIO_PUPD_NONE, GPIO8|GPIO9);
   gpio_set(GPIOC, GPIO8|GPIO9);
   
@@ -46,14 +32,7 @@ int main(void)
 
   while (1)
   {
-    //gpio_set(GPIOC, GPIO9);
-    
-    //cdcacm_manage();
-   /* if(tick_expire_ms(&main_tick, 1000)){
-
-      gpio_toggle(GPIOC, GPIO9);
-    }*/
-    
+   
   }
 
 }
@@ -62,8 +41,6 @@ void clock_init(void)
 {
   rcc_clock_setup_in_hsi_out_48mhz();
   rcc_periph_clock_enable(RCC_GPIOC);
- // rcc_periph_clock_enable(RCC_GPIOB);
-//  rcc_periph_clock_enable(RCC_AFIO);
 }
 
 
